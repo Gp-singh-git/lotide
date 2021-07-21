005feeda628df2749721ddaf08bf0a32a81a2d7c
gurpreet singh <gurpreet212004@gmail.com>
2021-07-21 19:18:50 +0000

    countonly file added

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x1F600) + String.fromCodePoint(0x1F600) + `Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(String.fromCodePoint(0x1F621) + String.fromCodePoint(0x1F621) +  `Assertion Failed: ${actual}  !==  ${expected}`);
+  }
+
+};
+const countOnly = function(nameList, validList) {
+  let finalObject = {};
+
+  for (let x in validList) {
+    let count = 0;
+    if (nameList.includes(x) && validList[x]) {
+      for (let y in nameList) {
+        if (nameList[y] === x) {
+          count += 1;
+        }
+      }
+      finalObject[x] = count;
+      count = 0;
+    }
+  }
+  return finalObject;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
