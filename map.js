60acc23ca222b20283c55ba2b6ebab62dac301f3
gurpreet singh <gurpreet212004@gmail.com>
2021-07-22 21:34:36 +0000

    added map function

---
+++ map.js
@@ -0,0 +1,36 @@
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array2.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(String.fromCodePoint(0x1F600) + String.fromCodePoint(0x1F600) + `Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(String.fromCodePoint(0x1F621) + String.fromCodePoint(0x1F621) +  `Assertion Failed: ${actual}  !==  ${expected}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+
+assertArraysEqual(map(words, word => word[0]), ['g','c','t','m','t']);
+assertArraysEqual(map(["apple", "banana", "grapes"], word => word[0]), ['a','b','g']);
+assertArraysEqual(map(["Canada", "Australia", "Germany"], word => word[0]), ['C','A','G']);
