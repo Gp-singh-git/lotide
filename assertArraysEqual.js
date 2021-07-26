7257da4aacda0dfc52087c216d949bbe0d1589ea
gurpreet singh <gurpreet212004@gmail.com>
2021-07-26 20:24:10 +0000

    modified middle

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array2.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -19,8 +7,5 @@ const assertArraysEqual = function(actual, expected) {
     console.log(String.fromCodePoint(0x1F621) + String.fromCodePoint(0x1F621) +  `Assertion Failed: ${actual}  !==  ${expected}`);
   }
 };
-  
 
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,3],[1,3,3]);
-assertArraysEqual([1,2,3],[1,2,"3"]);
+module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array2.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(String.fromCodePoint(0x1F600) + String.fromCodePoint(0x1F600) + `Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(String.fromCodePoint(0x1F621) + String.fromCodePoint(0x1F621) +  `Assertion Failed: ${actual}  !==  ${expected}`);
-  }
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -31,9 +9,4 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2,3,4,5,6,7]), [4]);
-assertArraysEqual(middle([1,2,3,4,5,6,7,8]), [4,5]);
-assertArraysEqual(middle([11,22,33,44]), [22,33]);
+module.exports = middle;
